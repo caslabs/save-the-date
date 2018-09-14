3af83aa9469a6bf63c58e34704b565eea51a5685
c0p <jeraldydcascayan@gmail.com>
2018-09-14 01:59:23 -1000

    ye done

---
+++ app.js
@@ -0,0 +1,9 @@
+document.body.addEventListener('click', function(e){
+    if (e.target.className === 'day') {
+       //when target is pointing to day boxes
+       var userTxt = document.createTextNode(window.prompt("New Event?","oof"));
+       var p = document.createElement("p");
+       p.appendChild(userTxt);
+       e.target.appendChild(p);
+    }
+});
